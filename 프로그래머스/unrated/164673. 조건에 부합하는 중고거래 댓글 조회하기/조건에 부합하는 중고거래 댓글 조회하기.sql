a944535978c5a395eb75626b709f60706dc408ea
Thinking Seo <110218894+seoinhyeok96@users.noreply.github.com>
2023-03-30 22:02:06 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,8 @@
+-- USED_GOODS_BOARD와 USED_GOODS_REPLY 테이블에서 2022년 10월에 작성된 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일을 조회하는 SQL문을 작성해주세요. 결과는 댓글 작성일을 기준으로 오름차순 정렬해주시고, 댓글 작성일이 같다면 게시글 제목을 기준으로 오름차순 정렬해주세요.
+
+
+SELECT B.TITLE, B.BOARD_ID, R.REPLY_ID, R.WRITER_ID, R.CONTENTS, DATE_FORMAT(R.CREATED_DATE, "%Y-%m-%d") AS CREATED_DATE
+FROM USED_GOODS_REPLY AS R
+JOIN USED_GOODS_BOARD AS B ON R.BOARD_ID = B.BOARD_ID
+WHERE DATE_FORMAT(B.CREATED_DATE, "%Y-%m") = "2022-10"
+ORDER BY R.CREATED_DATE ASC, B.TITLE ASC;
